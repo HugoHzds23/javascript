bd754fae61834895c682995cc7452da25e93f66f
Hugo Henrique Zacharias da Silva <eng.hugohenriquesilva@gmail.com>
2023-07-05 23:47:13 -0300

    Subindo Exercicios feitos

---
+++ ex008/ex008.js
@@ -0,0 +1,9 @@
+var vel = 100
+
+console.log(`A velocidade do seu carro é ${vel}kW/h`)
+console.log(`Dirija sempre usando cinto de segurança`)
+if (vel > 60){
+    console.log(`Vocẽ ultrapassou a velocidade permitida. MULTADO!!`)
+}else{
+    console.log(`Você esta dentro da velocidade permitida, Boa Viagem`)
+}
---
+++ ex008/ex009.js
@@ -0,0 +1,6 @@
+var pais = 'BRA'
+if (pais == 'BRA'){
+    console.log('Vocè é Brasileira')
+} else {
+    console.log('Vocé é Estrangeira')
+}
